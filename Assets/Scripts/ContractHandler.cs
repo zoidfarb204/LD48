1a8301a625b6afb0fc6dedc02c455b9bd5c1866b
Mike Rosenberger <mrosenberger@sciengineering.com>
2021-04-23 21:42:47 -0500

    Create and Update a contract prefab

---
+++ Assets/Scripts/ContractHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContractHandler : MonoBehaviour
+{
+    public GameObject contractPrefab;
+    private GameObject currentContract;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void DrawContact()
+    {
+        if (currentContract == null)
+        {
+            this.currentContract =
+                Instantiate(contractPrefab, new Vector3(0, 0, 0), Quaternion.identity, this.transform);
+            this.currentContract.transform.localPosition = new Vector3(0, 0, 0);
+
+            SetContract(this.currentContract);
+        }
+    }
+
+    private void SetContract(GameObject gameObject)
+    {
+        gameObject.transform.Find("Title").GetComponent<Text>().text = "New Title";
+        gameObject.transform.Find("Good").Find("GoodText").GetComponent<Text>().text = "Good Text Changed";
+        gameObject.transform.Find("Bad").Find("BadText").GetComponent<Text>().text = "Bad Text Changed";
+    }
+}
